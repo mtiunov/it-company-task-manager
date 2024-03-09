5e4ac16eee2018086dfacf8b93cdb11c1dada7b9
Mikhail <tiunovmixs@gmail.com>
2024-03-09 20:37:39 +0200

    Update task-manager-f5qd.onrender.com/ ALLOWED_HOSTS

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://task-manager-f5qd.onrender.com/"]
+ALLOWED_HOSTS = ["127.0.0.1", "task-manager-f5qd.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
